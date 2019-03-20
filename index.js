4fc963f8bb5048bb33177a5f27c3f8f913d5f8b5
cecilia1982 <ercolemc1982@gmail.com>
2019-03-19 21:56:01 -0300

    Integrando formularios y peticiones GET/POST

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
 const http = require("http")
 const fs = require("fs")
-const path = require ("path")
+const path = require("path")
 
-    http.createServer(function(request, response) {
+http.createServer(function(request, response) {
 
-        const dir = "public/" //<--DEFINIR EL DIRECTORIO DE LOS ARCHIVOS WEB
+    const dir = "public/" //<--DEFINIR EL DIRECTORIO DE LOS ARCHIVOS WEB
 
-        //const url = request.url // <-- LEER LA RUTA/RECURSO SOLICITADO EN LA URL
+    //const url = request.url // <-- LEER LA RUTA/RECURSO SOLICITADO EN LA URL
 
-        /*if( resquest.url == "/"){
-        	const file = "index.html"
-        } else {
-        	const file = request.url
-        }*/
+    /*if( resquest.url == "/"){
+        const file = "index.html"
+    } else {
+        const file = request.url
+    }*/
 
-        //const file = (CONDICION) ? VERDADERO : FALSO   //<-- OPERADOR TERNARIO
-        const file = (request.url == "/") ? "index.html" : request.url
-
-
-        let ext = path.extname(file)
-            ext = String(ext)
-            ext = ext.toLowerCase()
-        
-
-        let tipos = {
-			".html"	: "text/html",
-			".js"	: "text/javascript",
-			".css"	: "text/css",
-			".txt" 	: "text/plain",
-			".json"	: "application/json",
-			".png"	: "image/png",
-			".jpg"	: "image/jpg",
-			".gif"	: "image/gif",
-			".ico"	: "image/x-icon",
-			".wav"	: "audio/wav",
-			".mp4"	: "video/mp4",
-			".woff"	: "application/font-woff",
-			".ttf"	: "application/font-ttf",
-			".eot"	: "application/vnd.ms-fontobject",
-			".otf"	: "application/font-otf",
-			".svg"	: "application/image/svg+xml"
-        }
-
-        let contentType = tipos[ext] || "application/octet-stream" 
+    //const file = (CONDICION) ? VERDADERO : FALSO   //<-- OPERADOR TERNARIO
+    const file = (request.url == "/") ? "index.html" : request.url
 
 
-        console.log("Usted ah solicitado el archivo..." + file)
-        console.log("La extension del archivo es: " + ext)
+    if (file == "/enviar") {
 
-        fs.readFile(dir + file, function(error, content) { //<-- INTENTAR LEER/CARGAR EL ARCHIVO/RECURSO SOLICITADO
+        console.log("Este es el body de la peticion HTTP:")
 
-
-            if (error) { //<-- Si no pudo leer el archivo por ALGUNA x razon ...
-
-                response.end("ARCHIVO NO ENCONTRADO :(")
-
-            } else { //<-- SI EFECTIVAMENTE PUDO LEERLO...
-
-                response.writeHead(200, { "Content-Type": contentType }) //<-- ESPECIFICAR CODIGO DE RESPUESTA Y TIPO DE CONTENIDO
-
-                response.end(content) //<-- ENVIAR AL CLIENTE/NAVEGADOR EL ARCHIVO ENCONTRADO 
-            }
+        request.on("data", function(body){
+            let datos = body.toString()
+            console.log( datos )
 
         })
 
-    }).listen(80)
+        response.end("Mirar la consola de Git Bash")
+    }
+
+    let ext = path.extname(file)
+    ext = String(ext)
+    ext = ext.toLowerCase()
+
+
+    let tipos = {
+        ".html": "text/html",
+        ".js": "text/javascript",
+        ".css": "text/css",
+        ".txt": "text/plain",
+        ".json": "application/json",
+        ".png": "image/png",
+        ".jpg": "image/jpg",
+        ".gif": "image/gif",
+        ".ico": "image/x-icon",
+        ".wav": "audio/wav",
+        ".mp4": "video/mp4",
+        ".woff": "application/font-woff",
+        ".ttf": "application/font-ttf",
+        ".eot": "application/vnd.ms-fontobject",
+        ".otf": "application/font-otf",
+        ".svg": "application/image/svg+xml"
+    }
+
+    let contentType = tipos[ext] || "application/octet-stream"
+
+
+    console.log("Usted ah solicitado el archivo..." + file)
+    console.log("La extension del archivo es: " + ext)
+
+    fs.readFile(dir + file, function(error, content) { //<-- INTENTAR LEER/CARGAR EL ARCHIVO/RECURSO SOLICITADO
+
+
+        if (error) { //<-- Si no pudo leer el archivo por ALGUNA x razon ...
+
+            response.end("ARCHIVO NO ENCONTRADO :(")
+
+        } else { //<-- SI EFECTIVAMENTE PUDO LEERLO...
+
+            response.writeHead(200, { "Content-Type": contentType }) //<-- ESPECIFICAR CODIGO DE RESPUESTA Y TIPO DE CONTENIDO
+
+            response.end(content) //<-- ENVIAR AL CLIENTE/NAVEGADOR EL ARCHIVO ENCONTRADO 
+        }
+
+    })
+
+}).listen(80)
